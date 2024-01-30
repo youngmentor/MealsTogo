7a0e3f21b124e38204c001570c3ed4f3e80ed96b
youngmentor <olaleresuliton43@gmail.com>
2024-01-30 17:24:55 +0100

    added new changes

--- src/components/features/restaurants/components/restaurant-info.components.js
+++ src/components/features/restaurants/components/restaurant-info.components.js
@@ -1,11 +1,11 @@
 import React from "react-native";
 import { Card } from "react-native-paper";
-import { Text, StyleSheet } from "react-native";
+import { Text, View, Image } from "react-native";
 import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../../assets/star";
 import open from "../../../../../assets/open";
-
+import { Spacer } from "../../../spacer/spacer";
 const RestuarantsCard = styled(Card)`
   background-color: white;
 `;
@@ -29,7 +29,10 @@ const Title = styled.Text`
   color: ${(props) => props.theme.colors.text.primary};
   font-size: ${(props) => props.theme.fontSizes.title};
 `;
-const OpenTag = styled.View``;
+const OpenTag = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
 const StarAndOpenTag = styled.View`
   flex-direction: row;
   justify-content: space-between;
@@ -37,14 +40,14 @@ const StarAndOpenTag = styled.View`
 export const RestaurantsInfo = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -60,7 +63,17 @@ export const RestaurantsInfo = ({ restaurant = {} }) => {
             ))}
           </Rating>
           <OpenTag>
-            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red", fontSize: 12 }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
           </OpenTag>
         </StarAndOpenTag>
         <Address>{address}</Address>
